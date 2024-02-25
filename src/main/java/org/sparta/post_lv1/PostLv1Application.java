8b627b2f67c973dff13a1ea42c97dc8664bbf661
kyeongminRoh <rudalstpa11@nate.com>
2024-02-25 22:46:47 +0900

    Feat: "createdAt 기능추가"

--- src/main/java/org/sparta/post_lv1/PostLv1Application.java
+++ src/main/java/org/sparta/post_lv1/PostLv1Application.java
@@ -2,7 +2,9 @@ package org.sparta.post_lv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 꼭 달아줘야 createdAt기능 수행
 @SpringBootApplication
 public class PostLv1Application {
 
